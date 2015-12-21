7bcd5245911d453be487a149bbb0167a1f885e9f
Mekayla <mdmcadoo@udel.edu>
2015-12-21 01:30:51 -0500

    Final link...

---
+++ FinalExamQuestions/src/Employee.java
@@ -0,0 +1,14 @@
+public class Employee{
+	public static void main(String args[]) {
+		int a = 1;
+		int b = 2;
+		if ((a < 0) && (b++ < 3))
+		{
+		b++;
+		}
+		  
+		System.out.println(b);
+		}
+	
+}
+
