fd6f2f679ecb6605b8be81df1f6789e6d4e79664
Mekayla <mdmcadoo@udel.edu>
2015-12-21 01:41:56 -0500

    Final github link...

--- FinalExamQuestions/src/Employee.java
+++ FinalExamQuestions/src/Employee.java
@@ -1,14 +1,31 @@
+import java.io.*;
 public class Employee{
-	public static void main(String args[]) {
-		int a = 1;
-		int b = 2;
-		if ((a < 0) && (b++ < 3))
-		{
-		b++;
-		}
-		  
-		System.out.println(b);
-		}
+   private String name;
+   private int age;
+   private String designation;
+   private double salary;
 	
+   // This is the constructor of the class Employee
+   public Employee(String name){
+      this.name = name;
+   }
+   // Assign the age of the Employee  to the variable age.
+   public void empAge(int empAge){
+      age =  empAge;
+   }
+   /* Assign the designation to the variable designation.*/
+   public void empDesignation(String empDesig){
+      designation = empDesig;
+   }
+   /* Assign the salary to the variable	salary.*/
+   public void empSalary(double empSalary){
+      salary = empSalary;
+   }
+   /* Print the Employee details */
+   public void printEmployee(){
+      System.out.println("Name:"+ name );
+      System.out.println("Age:" + age );
+      System.out.println("Designation:" + designation );
+      System.out.println("Salary:" + salary);
+   }
 }
-
